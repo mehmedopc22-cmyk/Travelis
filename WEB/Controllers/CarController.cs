5608cc03a9a0f84a66951f5b1a6c4c0069e8db40
Mehmed <mehmedopc22@gmail.com>
2026-04-26 16:59:31 +0300

    Initial commit from PC

---
+++ WEB/Controllers/CarController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    public class CarsController : Controller
+    {
+        // 1. Начална страница за коли
+        public IActionResult Index() => View();
+
+        // 2. Списък с коли (Grid изглед)
+        public IActionResult Grid() => View();
+
+        // 3. Списък с коли (List изглед)
+        public IActionResult List() => View();
+
+        // 4. Детайли за конкретна кола
+        public IActionResult Details() => View();
+
+        // 5. Условия и политики
+        public IActionResult Policies() => View();
+
+        // 6. Форма за резервация
+        public IActionResult Booking() => View();
+
+        // 7. Обобщение на резервацията (Summary)
+        public IActionResult Summary() => View();
+
+        // 8. Успешна резервация
+        public IActionResult Success() => View();
+    }
+}
